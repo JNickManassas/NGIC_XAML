73e393329d28dfaf8b7c51f2d8b263890c95cc0a
Jim Nicholson <nicholsonsoftware@hotmail.com>
2020-05-18 07:02:46 -0400

    Add project files.

---
+++ NGIC/NGIC/Views/HomeDashboard/Promotion.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NGIC.Views.HomeDashboard
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Promotion : ContentPage
+    {
+        public Promotion()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NGIC/NGIC/Views/IDCards/BackSide.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NGIC.Views.IDCards
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BackSide : ContentPage
+    {
+        public BackSide()
+        {
+            InitializeComponent();
+        }
+    }
+}
